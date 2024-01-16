a9f32195cfabe20cfe8719f547ed4884dca620ce
Matt Bishop <mattsbishop1@gmail.com>
2024-01-15 20:25:15 -0700

    Added back in boost (movement is really slow otherwise).

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -58,7 +58,11 @@ namespace UnityTemplateProjects
 
         CameraState m_TargetCameraState = new CameraState();
         CameraState m_InterpolatingCameraState = new CameraState();
-        
+
+        [Header("Movement Settings")]
+        [Tooltip("Exponential boost factor on translation.")]
+        public float boost = 3.5f;
+
         [Tooltip("Time it takes to interpolate camera position 99% of the way to the target."), Range(0.001f, 1f)]
         public float positionLerpTime = 0.2f;
 
@@ -207,6 +211,9 @@ namespace UnityTemplateProjects
             {
                 translation *= 10.0f;
             }
+
+            // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
+            translation *= Mathf.Pow(2.0f, boost);
 
             m_TargetCameraState.Translate(translation);
 
