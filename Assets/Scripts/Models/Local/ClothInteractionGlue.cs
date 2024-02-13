dde2cda366963806b1d36e9b6c0c091f9a5b8b2a
Matt Bishop <mattsbishop1@gmail.com>
2024-02-12 21:31:46 -0700

    Got regular cloth simulation in and working! Not relaxation mode yet.

---
+++ Assets/Scripts/Models/Local/ClothInteractionGlue.cs
@@ -0,0 +1,40 @@
+using GrabTool.Mesh;
+using UnityEngine;
+using MeshHistory = GrabTool.Models.MeshHistory;
+
+namespace Models.Local
+{
+    public class ClothInteractionGlue : MonoBehaviour
+    {
+        [SerializeField] private MouseMeshDragger mouseMeshDragger;
+        [SerializeField] private MeshRenderer clothRenderer;
+        [SerializeField] private Material disabledMaterial;
+        [SerializeField] private Cloth.Behaviour.Cloth cloth;
+        [SerializeField] private MeshHistory meshHistory;
+        
+        private Material _previous;
+        
+        public void HandleDragComplete()
+        {
+            mouseMeshDragger.SetDisabled(true);
+            _previous = clothRenderer.material;
+            clothRenderer.material = disabledMaterial;
+            
+            cloth.MatchClothPositionsToMesh();
+            
+            cloth.ToggleSimulation();
+        }
+
+        public void HandleClothSimulationComplete()
+        {
+            // We need to undo and then add back the mesh back to the history so that the manipulation and simulation
+            // are counted as one thing in the history.
+            meshHistory.Undo();
+            var meshFilter = clothRenderer.gameObject.GetComponent<MeshFilter>();
+            meshHistory.AddMesh(meshFilter.sharedMesh);
+            
+            mouseMeshDragger.SetDisabled(false);
+            clothRenderer.material = _previous;
+        }
+    }
+}
