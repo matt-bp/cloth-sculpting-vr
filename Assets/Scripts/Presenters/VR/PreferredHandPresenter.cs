3c92c3a1ee2a28b44da91c004d6f4fd85e411959
Matt Bishop <mattsbishop1@gmail.com>
2024-02-15 00:58:39 -0700

    Got the right controller selecting the cloth with a dynamic input action reference binding.

--- Assets/Scripts/Presenters/VR/PreferredHandPresenter.cs
+++ Assets/Scripts/Presenters/VR/PreferredHandPresenter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Models.Global;
 using Models.Local;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 using Wright.Library.Helpers;
@@ -12,19 +13,28 @@ namespace Presenters.VR
 {
     public class PreferredHandPresenter : MonoBehaviour
     {
-        [SerializeField] private GameObject grabLeftController;
+        [Header("Controller Objects")] [SerializeField]
+        private GameObject grabLeftController;
+
         [SerializeField] private GameObject movementLeftController;
         [SerializeField] private GameObject leftControllerProgressParent;
         [SerializeField] private GameObject grabRightController;
         [SerializeField] private GameObject movementRightController;
         [SerializeField] private GameObject rightControllerProgressParent;
 
+        [Header("Miscellaneous things to update")]
         // Set these to the direct interactor game object component of the grab controller game object
-        [SerializeField] private FollowObject[] controllerColliderFollowers;
-        [SerializeField] private TaskProgressPositionsModel progressPositionsModel;
+        [SerializeField]
+        private FollowObject[] controllerColliderFollowers;
+
+        [Header("Dynamic Action References")]
+        [SerializeField] private InputActionReference grabActionReference;
+        [SerializeField] private InputActionReference example;
         
+        [Header("Model")] [SerializeField] private TaskProgressPositionsModel progressPositionsModel;
+
         public bool defaultToLeftHandGrab;
-        
+
         private void Start()
         {
             var models = GameObject.FindWithTag("Global Models");
@@ -41,12 +51,14 @@ namespace Presenters.VR
 
         private void ApplyPreference(bool isLeftHandGrab)
         {
+            SetActions(isLeftHandGrab);
+            
             SetGrabControllerState(grabLeftController, isLeftHandGrab);
             SetMovementControllerState(movementRightController, isLeftHandGrab);
-            
+
             SetGrabControllerState(grabRightController, !isLeftHandGrab);
             SetMovementControllerState(movementLeftController, !isLeftHandGrab);
-            
+
             progressPositionsModel.positions = isLeftHandGrab
                 ? rightControllerProgressParent.transform.Cast<Transform>().ToArray()
                 : leftControllerProgressParent.transform.Cast<Transform>().ToArray();
@@ -71,6 +83,14 @@ namespace Presenters.VR
             controller.SetActive(isEnabled);
             controller.GetComponent<ActionBasedControllerManager>().enabled = isEnabled;
             controller.GetComponent<ActionBasedController>().enabled = isEnabled;
+        }
+
+        private void SetActions(bool isLeftHandGrab)
+        {
+            var controller = isLeftHandGrab ? "LeftHand" : "RightHand";
+            
+            grabActionReference.action.AddBinding($"<XRController>{{{controller}}}/{{GripButton}}");
+            grabActionReference.action.bindingMask = new InputBinding { groups = "GenericXRController" };
         }
     }
 }
