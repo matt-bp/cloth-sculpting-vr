fdb3e478d6ae57b1406913504c754dffc030cfe5
Matt Bishop <mattsbishop1@gmail.com>
2024-03-05 21:18:00 -0700

    Updated the mouse wheel speed.

--- Assets/Scripts/Presenters/MouseWheelInteraction.cs
+++ Assets/Scripts/Presenters/MouseWheelInteraction.cs
@@ -1,26 +1,29 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using Wright.Library.Logging;
 
 namespace Presenters
 {
     public class MouseWheelInteraction : MonoBehaviour
     {
-        [SerializeField] private float _rate = 1.0f;
-        private float _start = 0.2f;
-        
+        private float _rate = 1.0f;
+        private float _currentValue = 0.2f;
+        [SerializeField] private float exponent = 1.0f;
         public UnityEvent<float> onUpdateSize;
-        
+
         private void Update()
         {
             if (Input.GetAxis("Mouse ScrollWheel") == 0) return;
-            
+
             var updated = Input.GetAxis("Mouse ScrollWheel") * _rate;
-            _start += updated;
 
-            _start = MathF.Max(0.01f, _start);
+            _currentValue += updated;
+            _currentValue = MathF.Max(0.01f, MathF.Min(1000.0f, _currentValue));
 
-            onUpdateSize.Invoke(_start);
+            // Scale our current value to grow quadratically.
+            var outValue = MathF.Pow(_currentValue, exponent);
+            onUpdateSize.Invoke(outValue);
         }
     }
 }
