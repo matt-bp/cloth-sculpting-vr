a19e64f9441ef9450127febd879ffc605e230948
Matt Bishop <mattsbishop1@gmail.com>
2024-03-15 17:29:06 -0600

    Now using the inverse camera rotation to move the axis. Need to adjust the initial rotation though.

--- Assets/Scripts/Temp/ApplyCameraOrientation.cs
+++ Assets/Scripts/Temp/ApplyCameraOrientation.cs
@@ -6,15 +6,18 @@ namespace UnityTemplateProjects.Temp
     public class ApplyCameraOrientation : MonoBehaviour
     {
         private Camera _camera;
+        private Quaternion _originalRotation;
 
         private void Start()
         {
             _camera = Camera.main;
+            _originalRotation = transform.rotation;
         }
 
         private void Update()
         {
-            transform.rotation = _camera.transform.rotation;
+            // do euler angles, and apply rotation?
+            transform.rotation = Quaternion.Inverse(_camera.transform.rotation);
         }
     }
 }
